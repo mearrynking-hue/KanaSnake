3d1e415b4c876cce3954a271b8e3d93fc5d64941
Polina <mearrynking@gmail.com>
2026-03-24 17:20:20 +0100

    Created WPF project, made layout for methods and added .gitignore

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KanaSnake;
+
+public partial class MainWindow : Window
+{
+    //variables
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    //start game button
+
+
+    //moving snake
+
+    
+    //spawning kana
+
+
+    //updating goal kana
+}
