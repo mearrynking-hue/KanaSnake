e17b68901204e62756fcf9dbf78f846cd1909d18
Polina <mearrynking@gmail.com>
2026-03-26 18:41:01 +0100

    Added controls for the snake and movement

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,8 +6,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace KanaSnake;
 
@@ -16,13 +18,13 @@ public partial class MainWindow : Window
     //variables
     private KanaManager _kanaManager = new KanaManager();
     private List<Snake> _snake = new List<Snake>();
-    private KanaPair _targetKana;
+    private KanaPair? _targetKana;
     private string _direction = "Right";
 
     public MainWindow()
     {
         InitializeComponent();
-
+        this.KeyDown += OnKeyDown;
         StartGame();
     }
 
@@ -31,8 +33,15 @@ public partial class MainWindow : Window
     {
         _snake.Clear();
         _snake.Add(new Snake(100, 100));
+
         _targetKana = _kanaManager.GetRandomKana();
         TargetText.Text = _targetKana.Romaji;
+
+        _gameTimer.Interval = TimeSpan.FromMilliseconds(150);
+        _gameTimer.Tick -= GameTick;
+        _gameTimer.Tick += GameTick;
+        _gameTimer.Start(); 
+        
         DrawSnake();
     }
 
@@ -53,14 +62,50 @@ public partial class MainWindow : Window
             };
 
             Canvas.SetLeft(rect, part.X);
-            Canvas.SetRight(rect, part.Y);
+            Canvas.SetTop(rect, part.Y);
 
             GameCanvas.Children.Add(rect);
         }
     }
 
-    //moving snake
+    //timer
+    private DispatcherTimer _gameTimer = new DispatcherTimer();
 
+    private void GameTick(object? sender, EventArgs e)
+    {
+        Move();
+        DrawSnake();
+    }
+
+    //moving snake
+    private void Move()
+    {
+        Snake head = _snake[0];
+        Snake newHead = new Snake(head.X, head.Y);
+
+        switch(_direction)
+        {
+            case "Up": newHead.Y -= 20; break;
+            case "Down": newHead.Y += 20; break;
+            case "Left": newHead.X -= 20; break;
+            case "Right": newHead.X += 20; break;
+        }
+
+        _snake.Insert(0, newHead);
+        _snake.RemoveAt(_snake.Count - 1);
+    }
+
+    //control(WASD)
+    private void OnKeyDown(object sender, KeyEventArgs e)
+    {
+        switch(e.Key)
+        {
+            case Key.W: if (_direction != "Down") _direction = "Up"; break;
+            case Key.S: if (_direction != "Up") _direction = "Down"; break;
+            case Key.A: if (_direction != "Right") _direction = "Left"; break;
+            case Key.D: if (_direction != "Left") _direction = "Right"; break;
+        }
+    }
     
     //spawning kana
 
